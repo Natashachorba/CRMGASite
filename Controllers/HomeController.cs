e907efc0978d4d890180b66c124fb7bbcda82111
NatashaDesktop <NatChorba@gmail.com>
2020-08-29 14:26:32 -0400

    Added all the site that exists up until this point

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CRM_SITE.Models;
+
+namespace CRM_SITE.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Background()
+        {
+            return View();
+        }
+
+        public IActionResult Description()
+        {
+            return View();
+        }
+
+        public IActionResult Training()
+        {
+            return View();
+        }
+
+        public IActionResult Research()
+        {
+            return View();
+        }
+
+        public IActionResult Partnerships()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
